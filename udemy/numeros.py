a3f6055b60f4bfe2befa5aa5282fff23b1b76859
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-12-27 19:46:37 -0300

    Start

---
+++ udemy/numeros.py
@@ -0,0 +1,19 @@
+# SUMA
+suma = 3+2
+print ("Suma: ",suma)
+
+# RESTA
+resta = 3-2
+print ("Resta: ",resta)
+
+# PRODUCTO
+producto = 9*9
+print ("Producto: ",producto)
+
+# DIVISION
+division = 10/2
+print ("Division: ",division) # Python muestra los flotantes con un punto "."
+
+# MODULO
+modulo = 9%2
+print ("Modulo: ", modulo)
