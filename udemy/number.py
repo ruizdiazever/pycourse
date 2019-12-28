94ca176102ca8ef7c91b13b7bc608e9ec629fe3a
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-12-27 21:13:52 -0300

    Add strings

---
+++ udemy/number.py
@@ -0,0 +1,35 @@
+# SUMA
+suma = 3+2
+print ("Suma: ",suma)
+
+# RESTA
+resta = 3-2
+print ("Resta: ",resta)
+
+# PRODUCTO
+producto = 9*9
+print ("Producto: ",producto)
+
+# DIVISION
+division = 10/2
+print ("Division: ",division) # Python muestra los flotantes con un punto "."
+
+# MODULO
+modulo = 9%2
+print ("Modulo: ", modulo)
+
+# POTENCIA
+potencia = 9**2
+print ("Potencia: ",potencia)
+
+# Asignacion de un valor a una variable
+n1 = 2
+n2 = 5
+n3 = n1+n2+10
+print("N°3: ",n3)
+
+# Nota de alumno
+nota_1 = 5.5
+nota_2 = 10
+nota_media = (nota_1 + nota_2) / 2 # Ojo con las reglas de precedencia
+print("Nota media alumno: ",nota_media)
---
+++ udemy/string.py
@@ -0,0 +1,35 @@
+texto_1 = "Hola mundo"
+texto_2 = 'Hola mundo'
+texto_3 = 'Este texto incluye unas "" '
+texto_4 = "Este texto incluye unas '' "
+texto_5 = "Esta \"palabra\" se encuentra escrita entre comillas"
+texto_6 = "Esta \'palabra\' se encuentra escrita entre comillas"
+
+print("Texto 1: ", texto_1)
+print("Texto 2: ", texto_2)
+print("Texto 3: ", texto_3)
+print("Texto 4: ", texto_4)
+print("Texto 5: ", texto_5)
+print("Texto 6: ", texto_6)
+print("\n")
+
+print("Una cadena")
+print("Otra cadena")
+print("Otra cadena mas")
+print("\n")
+
+print("Un texto\tuna tabulacion") # Tabulacion
+print("Un texto\nNueva linea") # Salto de linea
+print("\n")
+
+print("C:\nombre\directorio")
+print(r"C:\nombre\directorio") # Usando RAW mostramos en crudo
+print("\n")
+
+print("""Una linea
+otra linea
+otra linea\tuna tabulacion""")
+print("\n")
+
+c = "Esto es una cadena\ncon dos lineas"
+print (c)
