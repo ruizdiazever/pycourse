537ea7c823962ef45c9b530671b48106849446bf
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-12-27 21:57:03 -0300

    Avances

--- udemy/3_index_and_slicing.py
+++ udemy/3_index_and_slicing.py
@@ -9,3 +9,14 @@ print("Posicion 0 a 2: ",palabra[:2],"\n") # La ultima se excluye, que seria la 
 
 print("Posicion 2 al penultimo: ",palabra[2:-1]) # La ultima posicion se excluye, que seria "n"
 print("Posicion 2 al ultimo: ",palabra[2:],"\n")
+
+print("Palabra de principio a final: ",palabra[:])
+print("Palabra de principio a final: ", palabra[:2] + palabra[2:]+" (alternativo)", "\n")
+
+print("Un ejemplo exotico: ", palabra[:99])
+print("Otro ejemplo exotico: ", palabra[99:],"\n") # Esto no devuelve nada obviamente.
+
+palabra_2 = palabra
+palabra_2 = "N" + palabra_2[1:]
+print("La palabra 2 es: ", palabra_2,"\n")
+print("La cadena 'palabra_2' posee ",len(palabra_2), " caracteres") # Longitud de palabra_2
