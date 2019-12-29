74717d81dcc9a607c3b5c9c140f9e44754233dca
ruizdiazever <ruizdiaz.oe@gmail.com>
2019-12-29 14:19:09 -0300

    Changes

---
+++ udemy/5_input.py
@@ -0,0 +1,22 @@
+# EJEMPLOS 1
+valor = input("Introduzca un valor 1: ")
+print ("El valor es: ",valor)
+# EJEMPLO 1.1, convertir valor en entero
+valor = int(valor)
+suma = 5 + valor
+print("La suma del valor + 5 es: ",suma, "\n")
+
+# EJEMPLO 2
+valor_2 = input("Introduzca un valor 2: ")
+print ("El valor es: ",valor_2, "\n")
+
+# EJEMPLO 3
+valor_3 = input("Introduce un valor 3: ")
+print("El valor es: ",valor_3)
+
+# EJEMPLO 4, introducir numeros flotantes & sumarlos
+num_1 = float(input("Introduzca un numero con decimal: "))
+num_2 = float(input("Introduzca un numero con decimal: "))
+num_3 = float(input("Introduzca un numero con decimal: "))
+suma_float = num_1+num_2+num_3
+print("La suma de los numeros flotantes son: ", suma_float)
