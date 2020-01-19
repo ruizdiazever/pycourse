ae7ac8cb42bb93077f08e47a17d45262ac4f4764
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-01-19 08:57:07 -0300

    Add scripts

---
+++ udemy/5_entradas_salidas_datos/2_scripts.py
@@ -0,0 +1,6 @@
+# Guiones con instrucciones en codigo fuente que se ejecutan
+# de arriba a abajo, guardados en un fichero con un nombre
+# unico y ejecutados desde el interprete.
+
+# IMPORTANTE: pueden tomar datos (argumentos) en el momento de
+# la ejecucion.
