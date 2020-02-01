6c21293a5c40caed093f9f4b86d4d91c2d30c608
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-01-31 21:23:29 -0300

    Improvements

--- udemy/0_exercises/6_enunciados.py
+++ udemy/0_exercises/6_enunciados.py
@@ -5,6 +5,8 @@ print("EJERCICIO 1")
 def area_rectangulo(base,altura):
     area = base * altura
     print(area)
+    # alternativa
+    # return base * altura
 area_rectangulo(15,10)
 
 # 2) Realiza una función llamada area_circulo() que devuelva el área de un 
@@ -19,8 +21,8 @@ area_rectangulo(15,10)
 print("\nEJERCICIO 2")
 import math
 def area_circulo(radio):
-    print("El area del circulo es:",(radio**2)*math.pi)
-area_circulo(5)
+    return  (radio**2) * math.pi
+print(area_circulo(5))
 
 # 3) Realiza una función llamada relacion() que a partir de dos números cumpla lo siguiente:
 #       Si el primer número es mayor que el segundo, debe devolver 1.
@@ -30,22 +32,22 @@ area_circulo(5)
 print("\nEJERCICIO 3")
 def relacion(num_1, num_2):
     if num_1 > num_2:
-        print("Resultado:", 1)
+        return 1
     elif num_1 < num_2:
-        print("Resultado:", -1)
+        return -1
     elif num_1 == num_2:
-        print("Resultado:", 0)
-relacion(5,10)
-relacion(10,5)
-relacion(5,5)
+        return 0
+print(relacion(5,10))
+print(relacion(10,5))
+print(relacion(5,5))
 
 # 4) Realiza una función llamada intermedio() que a partir de dos números, devuelva su punto intermedio:
 #       Nota: El número intermedio de dos números corresponde a la suma de los dos números dividida entre 2
 #       Comprueba el punto intermedio entre -12 y 24
 print("\nEJERCICIO 4")
 def intermedio(num_1, num_2):
-    print((num_1+num_2) / 2)
-intermedio(-12,24)
+    return (num_1+num_2) / 2
+print(intermedio(-12,24))
 
 # 5) Realiza una función llamada recortar() que reciba tres parámetros. El primero es el número a recortar, 
 # el segundo es el límite inferior y el tercero el límite superior. La función tendrá que cumplir lo siguiente:
@@ -54,14 +56,14 @@ intermedio(-12,24)
 #       Devolver el número sin cambios si no se supera ningún límite.
 #       Comprueba el resultado de recortar 15 entre los límites 0 y 10
 print("\nEJERCICIO 5")
-def recortar(num_rec,lim_inf,lim_sup):
-    if num_rec < lim_inf:
-        print(lim_inf)
-    elif lim_sup > num_rec:
-        print(lim_sup)
+def recortar(num,minimo,maximo):
+    if num < minimo:
+        return minimo
+    elif maximo < num:
+        return maximo
     else:
-        print(num_rec)
-recortar(15,0,10)
+        return num
+print(recortar(15,0,10))
 
 
 # 6) Realiza una función separar() que tome una lista de números enteros y devuelva dos 
@@ -73,17 +75,17 @@ recortar(15,0,10)
     # Nota: Para ordenar una lista automáticamente puedes usar el método .sort().
 numeros = [-12, 84, 13, 20, -33, 101, 9]
 print("\nEJERCICIO 5")
-numeros.sort() # El metodo primero se usa fuera del print
-print(numeros) # Para luego tener el resultado..
-
-def separar(numeros):
+def separar(lista):
+    lista.sort()
     pares = []
     impares = []
-    for i in numeros:
+    for i in lista:
         if i % 2 == 0:
             pares.append(i)
         else:
             impares.append(i)
-    print("Pares: ",pares)
-    print("Impares: ",impares)
-separar(numeros)
+    return pares, impares
+
+pares, impares = separar(numeros) # Respetar el orden de retorno
+print("Pares:",pares)
+print("Impares:",impares)
