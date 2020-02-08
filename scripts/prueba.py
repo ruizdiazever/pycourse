5d267fcf4ac953e5b96efa4dafce362d29cad956
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-02-08 18:10:25 -0300

    Change

--- scripts/prueba.py
+++
@@ -1,3 +0,0 @@
-def test(num):
-    return num, num*2, num*4
-print(test(5))
---
+++ scripts/suma.py
@@ -0,0 +1,6 @@
+import sys
+
+num_1 = int(sys.argv[1])
+num_2 = int(sys.argv[2])
+
+print("La suma de los numeros es:",num_1+num_2)
