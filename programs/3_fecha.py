451d2f5120fee844506d6f4aabdb4dc221074faa
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-02-16 11:22:09 -0300

    Changes

--- programs/3_fecha.py
+++ programs/3_fecha.py
@@ -1,33 +1,24 @@
 # Pedir el día, mes & año de una fecha e indicar si la fecha es correcta. Suponiendo que todos los meses de 30 días.
 print("Ingrese porfavor la fecha.")
 
-try:
-    dia = int(input("Ingrese el dia: "))
-    mes = int(input("Ingrese el mes: "))
-    year = int(input("Ingrese el año: "))
+while(True):
+    try:
+        day = int(input("Ingrese el dia: "))
+        month = int(input("Ingrese el mes: "))
+        year = int(input("Ingrese el año: "))
 
-        # DIA
-    while (True):
-        if dia > 0 and dia < 30:
-            break
+        if (day > 0 and day <= 30) and (month > 0 and month <= 12) and (year > 0 and year < 3000):
+            print("La fecha ingresada es: {}/{}/{}".format(day,month,year))
         else:
-            dia = int(input("Error! Ingrese el dia correctamente: "))
-    # MES
-    while (True):
-        if mes > 0 and mes <= 12:
-            break
-        else:
-            mes = int(input("Error! Ingrese el mes correctamente: "))
-    # AÑO
-    while (True):
-        if year > 0 and year < 3000:
-            break
-        else:
-            year = int(input("Error! Ingrese el año correctamente: "))
-    print("La fecha ingresada es {}/{}/{}".format(dia, mes, year))
-except Exception as e:
-    print("ERROR!")
-    print(type(e).__name__)
+            print("ERROR! Fecha invalida.")
+
+    except ValueError:
+        print("ERROR! Introduzca un numero valido.")
+    except Exception as e:
+        print(type(e).__name__)
+    else:
+        break
+
 
 
 
