6a683ad48ca94317dbd0d58b6c3efbf637bb9da5
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-03-01 15:09:36 -0300

    Changes

---
+++ programs/5_vehiculo.py
@@ -0,0 +1,71 @@
+# Superclass
+class Vehiculo:
+    def __init__(self,tipo,motor,velocidad_max,finalidad):
+        self.tipo = tipo
+        self.motor = motor
+        self.velocidad_max = velocidad_max
+        self.finalidad = finalidad
+    def __str__(self):
+        return """\
+Tipo:               {}
+Motor:              {}
+Velocidad maxima:   {}
+Finalidad:          {}
+""".format(self.tipo,self.motor,self.velocidad_max,self.finalidad)
+
+# Class Auto herededa de superclase Vehiculo
+class Auto(Vehiculo):
+    desplazamiento = ""
+    descripcion = ""
+    material = ""
+    marca = ""
+    modelo = ""
+
+    def __str__(self):
+        return """
+Tipo:               {}
+Motor:              {}
+Velocidad maxima:   {}
+Finalidad:          {}
+Desplazamiento:     {}
+Descripcion:        {}
+Material:           {}
+Marca:              {}
+Modelo:             {}
+""".format(self.tipo,self.motor,self.velocidad_max,self.finalidad,self.desplazamiento,self.descripcion,self.material,self.marca,self.modelo)        
+
+auto = Auto("Automovil","Electrico","250 KM/H","Uso personal")
+auto.desplazamiento = "Terrestre"
+auto.descripcion = "Automovil con conduccion autonoma nivel 5, unica en su clase"
+auto.marca = "Tesla"
+auto.modelo = "Cybertruck"
+auto.material = "Aluminio endurecida en frio"
+print(auto)
+
+
+# Class Avion heredada de superclase Vehiculo
+class Avion(Vehiculo):
+    desplazamiento = ""
+    descripcion = ""
+    altura_vuelo = ""
+    fabricante = ""
+    modelo = ""
+
+    def __str__(self):
+        return """ 
+Tipo:               {}
+Motor:              {}
+Velocidad maxima:   {}
+Finalidad:          {}
+Desplazamiento:     {}
+Altura maxima:      {}
+Fabricante:         {}
+Modelo:             {}        
+""".format(self.tipo,self.motor,self.velocidad_max,self.finalidad,self.desplazamiento,self.altura_vuelo,self.fabricante,self.modelo)
+
+avion = Avion("Aeronave","Reaccion","Cruzero","Transporte de pasajeros")
+avion.altura_vuelo = "15 KM"
+avion.fabricante = "Airbus"
+avion.modelo = "A380"
+avion.desplazamiento = "Aereo"
+print(avion)
--- udemy/FASE 3 - Programacion orientada a objetos/09 Herencia/herencia.py
+++ udemy/FASE 3 - Programacion orientada a objetos/09 Herencia/herencia.py
@@ -3,21 +3,96 @@
 # Relacion Superclase > subclase
 
 
+# SIN HERENCIA: INEFICIENTE
+
+# class Producto: 
+    #def __init__(self,referencia,tipo,nombre,pvp,descripcion,productor=None,distribuidor=None,isbn=None,autor=None):
+        #self.referencia = referencia
+        #self.tipo = tipo
+        #self.nombre = nombre
+        #self.pvp = pvp
+        #self.descripcion = descripcion
+        #self.productor = productor
+        #self.distribuidor = distribuidor
+        #self.isbn = isbn
+        #self.autor = autor
+    
+#adorno = Producto('000A','ADORNO','Vaso adornado',15,'Vaso de porcelana con dibujos')
+
+# Para consultas
+#print(adorno.tipo)          # Tipo
+#print(adorno.pvp)           # Precio de venta al publico
+#print(adorno.descripcion)   # Descripcion
+
+
+
+
+# CON HERENCIA:
+# 1. SUPERCLASE
 class Producto:
-    def __init__(self,referencia,tipo,nombre,pvp,descripcion,productor=None,distribuidor=None,isbn=None,autor=None):
+    def __init__(self,referencia,nombre,pvp,descripcion):
         self.referencia = referencia
-        self.tipo = tipo
         self.nombre = nombre
         self.pvp = pvp
         self.descripcion = descripcion
-        self.productor = productor
-        self.distribuidor = distribuidor
-        self.isbn = isbn
-        self.autor = autor
-    
-adorno = Producto('000A','ADORNO','Vaso adornado',15,'Vaso de porcelana con dibujos')
+    def __str__(self):
+        # Con diagonal le indicanos que no comience con espacio.
+        # Dejando lo demas a la izquierda indicamos la posicion.
+        return """\
+REFERENCIA:   {}
+NOMBRE:       {}
+PVP:          {}
+DESCRIPCION:  {}
+        """.format(self.referencia,self.nombre,self.pvp,self.descripcion)
 
-# Para consultas
-print(adorno.tipo)          # Tipo
-print(adorno.pvp)           # Precio de venta al publico
-print(adorno.descripcion)   # Descripcion
+
+
+
+# 2. SUBCLASE SIN ATRIBUTOS EXTRAS
+class Adorno(Producto):
+    pass
+a = Adorno(2034,"Vaso adornado",15,"Vaso de porcelana adornado con arboles")
+print(a)
+
+
+
+
+# 3. SUBCLASE CON ATRIBUTOS NUEVOS, con asignacion externa.
+class Alimento(Producto):
+    productor = ""
+    distribuidor = ""
+
+    def __str__(self):
+        return """\
+REFERENCIA:   {}
+NOMBRE:       {}
+PVP:          {}
+DESCRIPCION:  {}
+PRODUCTOR:    {}
+DISTRIBUIDOR: {}
+        """.format(self.referencia,self.nombre,self.pvp,self.descripcion,self.productor,self.distribuidor)
+
+al = Alimento(2035,"Leche descremada",5,"250ml en envase de vidrio.")
+al.productor = "La Serenisima"
+al.distribuidor = "Logistica Tesla SA"
+print(al)
+
+# 4. SUBCLASE CON ATRIBUTOS NUEVOS, con asignacion externa.
+class Libro(Producto):
+    isbn = ""
+    autor = ""
+
+    def __str__(self):
+        return """\
+REFERENCIA:   {}
+NOMBRE:       {}
+PVP:          {}
+DESCRIPCION:  {}
+ISBN:         {}
+AUTOR:        {}
+        """.format(self.referencia,self.nombre,self.pvp,self.descripcion,self.isbn,self.autor)
+
+li = Libro(2077,"La Fundacion",5,"Saga emblematica de la ciencia ficcion.")
+li.isbn = "978-987-566-112-7"
+li.autor = "Isac Asimov"
+print(li)
