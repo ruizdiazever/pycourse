d96f2e151ae97e0be8d011fe32e19b6101f8ab2a
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-03-16 16:25:51 -0300

    Changes

--- udemy/FASE 4 - Temas avanzados/11 Modulos/1_modulos/test/test.py
+++ udemy/FASE 4 - Temas avanzados/11 Modulos/1_modulos/test/test.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.path.insert(1,'D:/Personal/Google Drive/dev/technologies/python/udemy/FASE 4 - Temas avanzados/11 Modulos') # '..' hace referencia a un directorio anterior..
+sys.path.insert(1,'D:/Personal/Google Drive/dev/technologies/python/udemy/FASE 4 - Temas avanzados/11 Modulos/1_modulos') # '..' hace referencia a un directorio anterior..
 # print(sys.path) # Con esto mostramos la ruta donde Python busca el modulo..
 
 #   Esto da error sin la especificacion de directorio, ya que Python no encuentra el modulo..
