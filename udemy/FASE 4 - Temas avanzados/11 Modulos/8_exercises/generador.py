ecc0021062a3ff3805dfa7f185ffcca46234d1c0
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-03-21 05:45:16 -0300

    Changes

--- udemy/FASE 4 - Temas avanzados/11 Modulos/8_exercises/generador.py
+++ udemy/FASE 4 - Temas avanzados/11 Modulos/8_exercises/generador.py
@@ -16,7 +16,7 @@ import math
 def leer_numero(inicio,fin,mensaje):
     while True:
         try:
-            valor = int( input(mensaje) )
+            valor = int(input(mensaje))
         except:
             pass
         else:
@@ -26,21 +26,22 @@ def leer_numero(inicio,fin,mensaje):
 
 
 def generador():
-    numeros = leer_numero(1,20,"¿Cuantos números quieres generar del 1 al 20?")
-    modo = leer_numero(1,3,"¿Cómo quieres redondear los números? 1: alza, 2: baja, 3: normal")
+    numeros = leer_numero(1,20,"¿Cuantos números quieres generar del 1 al 20?\n")
+    modo = leer_numero(1,3,"¿Cómo quieres redondear los números? 1: alza, 2: baja, 3: normal\n")
 
     lista = []
 
+    print("\n")
     for i in range(numeros):
         num = random.uniform(0,101)
         if modo == 1:
-            print("{} => {}".format(num, math.ceil(num)))
+            print(i+1,"- {} => {}".format(num, math.ceil(num)))
             num = math.ceil(num)
         elif modo == 2:
-            print("{} => {}".format(num, math.floor(num)))
+            print(i+1, "- {} => {}".format(num, math.floor(num)))
             num = math.floor(num)
         else:
-            print("{} => {}".format(num, round(num)))
+            print(i+1, "- {} => {}".format(num, round(num)))
             num = round(num)
         lista.append(num)
     return lista
