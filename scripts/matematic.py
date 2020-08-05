051a652cb0c633af65ae170000e751d117062d1c
ruizdiazever <ruizdiaz.oe@gmail.com>
2020-08-05 15:00:04 -0300

    Clean code, details

--- scripts/matematic.py
+++ scripts/matematic.py
@@ -4,19 +4,19 @@ class Matematica:
     def suma(self, num_1, num_2):
         self.num_1 = num_1
         self.num_2 = num_2
-        print("\nLa suma de ambos numeros es:",num_1+num_2)
+        print("\nLa suma de ambos numeros es:", num_1 + num_2)
     def resta(self, num_1, num_2):
         self.num_1 = num_1
         self.num_2 = num_2
-        print("La resta de ambos numeros es:",num_1-num_2)
+        print("La resta de ambos numeros es:", num_1 - num_2)
     def producto(self, num_1, num_2):
         self.num_1 = num_1
         self.num_2 = num_2
-        print("El producto de ambos numeros es:",num_1*num_2)
+        print("El producto de ambos numeros es:", num_1 * num_2)
     def division(self, num_1, num_2):
         self.num_1 = num_1
         self.num_2 = num_2
-        print("La division de ambos numeros es:",num_1/num_2)
+        print("La division de ambos numeros es:", num_1 / num_2)
 
 # Instanciamos la clase
 objeto_matematico = Matematica()
@@ -26,10 +26,10 @@ while(True):
         num_1 = int(input("1. Ingrese el primer valor: "))
         num_2 = int(input("2. Ingrese el segundo valor: "))
 
-        objeto_matematico.suma(num_1,num_2)
-        objeto_matematico.resta(num_1,num_2)
-        objeto_matematico.producto(num_1,num_2)
-        objeto_matematico.division(num_1,num_2)
+        objeto_matematico.suma(num_1, num_2)
+        objeto_matematico.resta(num_1, num_2)
+        objeto_matematico.producto(num_1, num_2)
+        objeto_matematico.division(num_1, num_2)
         break
     except ValueError:
         print("Error! Introduzca un numero.")
---
+++ sebas.py
@@ -0,0 +1,9 @@
+# EJERCICIO 1.1
+    # EJERCICIO A
+name = input("Escriba su nombre: ")
+
+print(f"""\n
+###################################
+Bienvenido {name} a la Matrix.
+###################################
+""")
